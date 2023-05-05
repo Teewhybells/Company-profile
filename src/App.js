9714ea83382eb47d2383dd05ae59e19e27ecb4a4
oluwatoyinbello <teewhybells@gmail.com>
2023-05-05 20:42:08 +0100

    make a change to main branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import './App.css';
-import { useState } from 'react';
-import SearchTextfield from './components/search_textfield'
-import ProfileResult from './components/ProfileResult';
-import profileJson from './json/profiles.json'
-
 function App() {
-  const [profiles, setProfiles] = useState(profileJson)
-  const getProfiles = (data) => setProfiles(data);
-  
-  return (
-    <div className="App">
-      <SearchTextfield onKeyDown={getProfiles}/>
-      <ProfileResult profiles={profiles.length === 0 ? profileJson : profiles }/>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
